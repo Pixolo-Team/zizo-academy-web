d285666d53810edefef6ba745860135e9b9b2001
Sameen Solkar <sameen.pixolo@gmail.com>
2025-08-06 17:19:31 +0530

    fix:remove neometric font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,57 +3,6 @@ import loadFont from "next/font/local";
 import "./globals.css";
 
 // Fetch Fonts
-const neometricFont = loadFont({
-  src: [
-    {
-      path: "../../public/fonts/neometric/neometric-hairline.woff",
-      weight: "100",
-      style: "hairline",
-    },
-    {
-      path: "../../public/fonts/neometric/neometric-extra-light.woff",
-      weight: "200",
-      style: "extra-light",
-    },
-    {
-      path: "../../public/fonts/neometric/neometric-light.woff",
-      weight: "300",
-      style: "light",
-    },
-    {
-      path: "../../public/fonts/neometric/neometric-regular.woff",
-      weight: "400",
-      style: "regular",
-    },
-    {
-      path: "../../public/fonts/neometric/neometric-medium.woff",
-      weight: "500",
-      style: "medium",
-    },
-    {
-      path: "../../public/fonts/neometric/neometric-bold.woff",
-      weight: "600",
-      style: "bold",
-    },
-    {
-      path: "../../public/fonts/neometric/neometric-extra-bold.woff",
-      weight: "700",
-      style: "extra-bold",
-    },
-    {
-      path: "../../public/fonts/neometric/neometric-black.woff",
-      weight: "800",
-      style: "black",
-    },
-    {
-      path: "../../public/fonts/neometric/neometric-heavy.woff",
-      weight: "900",
-      style: "heavy",
-    },
-  ],
-  variable: "--font-neometric",
-  display: "swap",
-});
 
 const gtWalsheim = loadFont({
   src: [
@@ -163,7 +112,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${neometricFont.variable} ${gtWalsheim.variable}`}
+      className={`${gtWalsheim.variable}`}
     >
       <body className="antialiased font-sans">{children}</body>
     </html>
