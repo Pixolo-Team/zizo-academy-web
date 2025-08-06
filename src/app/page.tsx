2aebfac629a492d485caa110deee67b1b4520f74
Sameen Solkar <sameen.pixolo@gmail.com>
2025-08-06 18:41:56 +0530

    fix:add comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+// COMPONENTS //
 import Image from "next/image";
 
+/** Home Page */
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
