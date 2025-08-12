6f6c068eea242ad2b7a1be265bcf2ed6e1b5a488
Sameen Solkar <sameen.pixolo@gmail.com>
2025-08-12 16:39:05 +0530

    feat: add error state

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -7,7 +7,14 @@ import { Input } from "@/components/ui/input";
 // NEXT //
 import Link from "next/link";
 
+// REACT //
+import { useState } from "react";
+
+/** Login Page */
 export default function LoginPage() {
+  // States
+  const [isError, setIsError] = useState<boolean>(false);
+
   return (
     // Main container
     <div className="h-screen flex flex-col gap-16 pl-14 pr-14 pt-24 pb-24 justify-start items-center">
@@ -20,7 +27,7 @@ export default function LoginPage() {
       <div className="flex flex-col gap-6 justify-center items-center">
         {/* Form header */}
         <div className="flex flex-col justify-center items-center">
-          <span className="text-2xl font-bold text-n-900">
+          <span className="text-2xl font-semibold text-n-900">
             Kick off your journey
           </span>
           <span className="text-base font-normal text-n-700">
@@ -30,17 +37,29 @@ export default function LoginPage() {
 
         {/* Form body */}
         <div className="w-full flex flex-col justify-center items-center">
-          {/* Phone number input */}
-          <Input
-            id="phone"
-            type="tel"
-            placeholder="Enter your phone number"
-            className="mb-5"
-          />
+          <div className="w-full mb-5 flex flex-col gap-1 justify-center items-center">
+            {/* Phone number input */}
+            <Input
+              id="phone"
+              type="tel"
+              placeholder="Enter Phone Number"
+              className={`py-6 ps-3 pe-[56px] ${
+                isError ? "border-red-500" : ""
+              }`}
+            />
+            {isError && (
+              <p className="text-sm text-red-500">
+                The phone number you entered is not a Zizo user
+              </p>
+            )}
+          </div>
           {/* Lets Play button and sign up link container */}
           <div className="w-full flex flex-col gap-10 justify-center items-center">
             {/* Lets Play button */}
-            <Button className="w-full h-13 rounded-full" type="submit">
+            <Button
+              className="w-full h-13 rounded-full  text-lg font-medium"
+              type="submit"
+            >
               Lets Play
             </Button>
             {/* Sign up link */}
