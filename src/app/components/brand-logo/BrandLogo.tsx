91bdcb33b086e54a6c7683df1d12dd9878aa5681
Sameen Solkar <sameen.pixolo@gmail.com>
2025-08-12 16:54:14 +0530

    fix:pr comments

--- src/app/components/brand-logo/BrandLogo.tsx
+++ src/app/components/brand-logo/BrandLogo.tsx
@@ -6,8 +6,8 @@ import Image from "next/image";
 
 // Interface for the BrandLogo component */
 interface BrandLogoProps {
-  variant?: Variant;
-  size?: string;
+  variant: Variant;
+  size?: number;
 }
 
 // Type for the variant prop
@@ -16,19 +16,16 @@ type Variant = "color" | "white" | "color-icon" | "white-icon";
 /** Brand Logo Component */
 const BrandLogo: React.FC<BrandLogoProps> = ({
   variant = "color",
-  size = "auto",
+  size = 20,
 }) => {
   // The path to the SVG logo image
   const imagePath = `/brand-logo/${variant}.svg`;
 
-  // The size classes for the logo
-  const sizeClasses = size || "w-auto h-auto";
-
   return (
     // The container for the logo
-    <div className={`relative ${sizeClasses}`}>
+    <div className={`size-${size}`}>
       {/* The image element */}
-      <Image src={imagePath} alt="Brand Logo" fill priority />
+      <Image src={imagePath} alt="Brand Logo" width={188} height={188} />
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,5 @@ import BrandLogo from "@/app/components/brand-logo/BrandLogo";
 
 /** Home Page */
 export default function Home() {
-  return <BrandLogo variant="color" size="w-20 h-20" />;
+  return <> </>;
 }
