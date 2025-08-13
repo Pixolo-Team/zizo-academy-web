80bc8ccb6a06fd09f3259aba2911f7cc64a1c402
omisan01 <omkar12mitake@gmail.com>
2025-08-13 20:29:08 +0530

    pr fixes

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -40,8 +40,8 @@ export default function LoginPage() {
   return (
     // Main container
     <div
-      className="h-screen bg-n-50 flex flex-col gap-[58px] px-14 py-24 justify-start items-center
-    max-md:px-5 max-md:py-14 max-md:gap-11"
+      className="h-screen  bg-n-50 flex flex-col gap-[58px] px-14 py-24 justify-start items-center
+    max-md:px-5 max-md:py-14 max-md:gap-11 m-auto"
     >
       {/* Logo container */}
       <div className="flex flex-col justify-start items-center">
@@ -50,7 +50,7 @@ export default function LoginPage() {
       </div>
 
       {/* Form container */}
-      <div className="w-6/25 flex flex-col gap-6 justify-center items-center max-xl:w-6/10 max-md:w-full">
+      <div className="flex flex-col gap-6 justify-center items-center w-full">
         {/* Form header */}
         <div className="flex flex-col justify-center items-center">
           <p className="text-2xl font-bold text-n-900">Kick off your journey</p>
@@ -60,20 +60,22 @@ export default function LoginPage() {
         </div>
 
         {/* Form body */}
-        <div className="w-full flex flex-col justify-center items-center">
+        <div className="w-[450px] max-sm:w-full flex flex-col justify-center items-center">
           <div className="w-full mb-5 flex flex-col gap-1">
             {/* Phone number input */}
             <Input
               id="phone"
               type="tel"
               placeholder="Enter Phone Number"
-              className={`py-6 pr-3 pr-14 border-n-300 text-lg h-13 placeholder:text-n-400 ${
+              className={`py-6 border-n-300 text-lg placeholder:text-n-400 ${
                 phoneNumberErrorMessage ? "border-red-500" : ""
               }`}
               value={phoneNumber}
               onChange={(e) => setPhoneNumber(e.target.value)}
             />
-            {phoneNumberErrorMessage && (
+
+            {/* Phone Number Error Message  */}
+            {phoneNumberErrorMessage !== "" && (
               <p className="text-sm text-red-500">{phoneNumberErrorMessage}</p>
             )}
           </div>
