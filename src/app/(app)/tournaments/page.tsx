d35f6227566ecae5a173feec46beb1c949e483d6
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2025-12-22 18:50:21 +0530

    Tournament card component

--- src/app/(app)/tournaments/page.tsx
+++ src/app/(app)/tournaments/page.tsx
@@ -2,6 +2,7 @@
 
 // COMPONENTS //
 import Motion from "@/components/animations/Motion";
+import TournamentCard from "@/components/tournaments/TournamentCard";
 import PageHeader from "@/components/ui/PageHeader";
 
 // OTHERS //
@@ -23,10 +24,31 @@ export default function Tournaments() {
   return (
     // Tournaments Listing Page
     <section>
-      <div className="container mx-auto px-6 py-7">
+      <div className="container mx-auto px-6 py-7 flex flex-col gap-5">
         <Motion as="div" variants={shrinkIn} delay={0.1}>
           {/* PageHeader component */}
           <PageHeader title="Find local football tournaments near you." />
+        </Motion>
+
+        {/* Tournaments Grid */}
+        <Motion as="div" variants={shrinkIn} delay={0.2}>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <TournamentCard
+              src="/images/defaults/tournament-card-dummy.png"
+              title="Mumbai Super League"
+              location="Ghatkopar, Mumbai"
+              price="₹2,500"
+              btnText="View Tournament"
+              badgeItems={[
+                "Prize Pool",
+                "1st Hello team",
+
+                "10 Teams",
+                "2nd Place",
+                "3rd Place",
+              ]}
+            />
+          </div>
         </Motion>
       </div>
     </section>
